5e0e3e559edb27f5d4851a6296a57ec26a4c6e48
Clement Hoang <clement.hoang24@gmail.com>
2015-01-31 02:53:27 -0500

    Adds application layout

--- client/routes/router.js
+++ client/routes/router.js
@@ -1,7 +1,11 @@
-Router.route('/login', function() {
-    this.render('login');
+Router.configure({
+    layoutTemplate: 'applicationLayout'
 });
 
 Router.route('/', function() {
     this.render('dashboard');
+});
+
+Router.route('/login', function() {
+    this.render('login');
 });
