306ed0e942ac40e85fd97c68d518fc3ec5336ed1
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-31 06:49:06 -0500

    Improving search logic.

--- client/events/search.js
+++ client/events/search.js
@@ -2,8 +2,13 @@ Template.search.events({
 	'input #searchBox > input': function(e, templateInstance) {
 		e.preventDefault();
 		var searchString = templateInstance.find("#searchInput").value;
-		Session.set('searchResults', Meteor.users.find({
-			username: new RegExp('^' + searchString + '.*$')
-		}).fetch());
+
+		if (searchString) {
+			Session.set('searchResults', Meteor.users.find({
+				username: new RegExp("^" + searchString + '.*$')
+			}).fetch());
+		} else {
+			Session.set('searchResults', []);
+		}
 	}
 })
