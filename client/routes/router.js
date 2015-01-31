3ef1d8c43403a72b58a6945e1a73fa2c80748cc9
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-31 04:13:28 -0500

    Redirect to home if the user is authenticated and tries to login

--- client/routes/router.js
+++ client/routes/router.js
@@ -1,13 +1,17 @@
 Router.configure({
-    layoutTemplate: 'applicationLayout'
+	layoutTemplate: 'applicationLayout'
 });
 
 Router.route('/home', function() {
-    this.render('dashboard', {to: 'content'});
+	this.render('dashboard', {to: 'content'});
 });
 
 Router.route('/', function() {
-	this.layout('landingLayout');
-	this.render('register', {to: 'registration'});
-    this.render('login', {to: 'login'});
+	if (!Meteor.userId()) {
+		this.layout('landingLayout');
+		this.render('register', {to: 'registration'});
+		this.render('login', {to: 'login'});
+	} else {
+		this.redirect('/home');
+	}
 });
