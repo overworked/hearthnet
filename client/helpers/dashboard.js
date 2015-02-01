5e215749c19d16cd406e2a828499669394993cd1
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-31 23:54:05 -0500

    Adding other players to dashboard page.

--- client/helpers/dashboard.js
+++ client/helpers/dashboard.js
@@ -1,5 +1,8 @@
 Template.dashboard.helpers({
 	featuredUsers: function(){
 		return Meteor.users.find({}, {limit:3}); //TODO: implement better logic to find featured users & restrict count
+	},
+	normalUsers: function(){
+		return Meteor.users.find({}, {limit: 8});
 	}
 });
