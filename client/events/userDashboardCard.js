33c492ff3410542964c078f7b74c6d18c00f3fff
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-02-01 04:25:31 -0500

    Clicking on the button with the user's name toggles the black transparent background on top of it.

---
+++ client/events/userDashboardCard.js
@@ -0,0 +1,5 @@
+Template.userDashboardCard.events({
+	'click a.user-dashboard-card-button': function(e, templateInstance) {
+		$(templateInstance.find('.dashboard-card-overlay')).toggleClass('visible');
+	}
+});
