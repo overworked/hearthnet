2ebf9157060a63654e033539649284a10dd729e7
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-31 20:16:26 -0500

    Correcting translation comma and space delimited string into an array.

--- client/events/editAccount.events.js
+++ client/events/editAccount.events.js
@@ -6,9 +6,9 @@ Template.editAccount.events({
         var ign = templateInstance.find('#ign').value;
         var description = templateInstance.find('#description').value;
         var email = templateInstance.find('#email').value;
-        var preferredDecksToFace = templateInstance.find('#preferredDecksToFaceTags').value.split(',').filter(function(e){return e;});
-        var dislikedDecksToFace = templateInstance.find('#dislikedDecksTags').value.split(',').filter(function(e){return e;});
-        var decksUsed = templateInstance.find('#decksUsed').value.split(',').filter(function(e){return e;});
+        var preferredDecksToFace = templateInstance.find('#preferredDecksToFaceTags').value.split(',').join(' ').split(' ').filter(function(e){return e;});
+        var dislikedDecksToFace = templateInstance.find('#dislikedDecksTags').value.split(',').join(' ').split(' ').filter(function(e){return e;});
+        var decksUsed = templateInstance.find('#decksUsed').value.split(',').join(' ').split(' ').filter(function(e){return e;});
 
         var updatedFields = {
             username : username,
